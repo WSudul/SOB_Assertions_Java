9fce3b6cff296d78de4fc9ca99ca0377fdad0935
WSudul <wojciech.sudul@gmail.com>
2018-05-16 00:45:43 +0200

    empty implementation of interface

---
+++ src/number/NumberSetImpl.java
@@ -0,0 +1,37 @@
+package number;
+
+public class NumberSetImpl implements NumberSet {
+    @Override
+    public void add(int i) throws Exception {
+    }
+
+    @Override
+    public void remove(int i) throws Exception {
+
+    }
+
+    @Override
+    public int getRandomValue() throws Exception {
+        return 0;
+    }
+
+    @Override
+    public int getSumOfElements() throws Exception {
+        return 0;
+    }
+
+    @Override
+    public void divideAllElementsBy(int d) throws Exception {
+
+    }
+
+    @Override
+    public boolean contain(int i) {
+        return false;
+    }
+
+    @Override
+    public int getSize() {
+        return 0;
+    }
+}
